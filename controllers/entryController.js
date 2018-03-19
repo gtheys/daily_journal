964fd86ac11f33dcea5b2a9b6cfeb2b95898895e
Geert Theys <geert.theys@gmail.com>
2018-03-19 16:29:17 +0700

    added some empty Entry controllers
    Next todo :)

--- controllers/entryController.js
+++ controllers/entryController.js
@@ -5,3 +5,18 @@ exports.entry_list = function(req, res) {
     res.render('entry_list', { title: 'Entries', message: Entries.entries().entries });
 };
 
+// Display detail page for a specific Entry.
+exports.entry_detail = function(req, res) {
+    res.send('NOT IMPLEMENTED: Entry detail: ' + req.params.id);
+};
+
+// Display Author create form on GET.
+exports.entry_create_get = function(req, res) {
+    res.send('NOT IMPLEMENTED: Entry create GET');
+};
+
+// Handle Author create on POST.
+exports.entry_create_post = function(req, res) {
+    res.send('NOT IMPLEMENTED: Entry create POST');
+};
+
