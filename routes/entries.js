aba2263ef3bf928dfa40e58bc247cf9aad358090
Geert Theys <geert.theys@gmail.com>
2018-03-19 16:33:22 +0700

    added routes for more entry manipulation

--- routes/entries.js
+++ routes/entries.js
@@ -5,8 +5,16 @@ let router = express.Router();
 
 const entryController = require('../controllers/entryController');
 
+// GET request for creating Entry. NOTE This must come before route for id (i.e. display entry).
+router.get('/entry/create', entryController.entry_create_get);
+
+// POST request for creating Author.
+router.post('/entry/create', entryController.entry_create_post);
+
+// GET request for one Author.
+router.get('/entry/:id', entryController.entry_detail);
+
 // Try to route to my entry_list
 router.get('/', entryController.entry_list);
-
 
 module.exports = router;
