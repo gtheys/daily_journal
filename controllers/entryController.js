a97d84ccbe38db582c1e37e53a7e7a0179e201c3
Geert Theys <geert.theys@gmail.com>
2018-03-19 16:11:34 +0700

    show journal entries in view

--- controllers/entryController.js
+++ controllers/entryController.js
@@ -2,6 +2,6 @@ let Entries = require('../models/entries');
 
 // Display all Entries
 exports.entry_list = function(req, res) {
-    res.render('entry_list', {title: 'Hey', message: Entries.entries()});
+    res.render('entry_list', { title: 'Entries', message: Entries.entries().entries });
 };
 
--- models/entries.js
+++ models/entries.js
@@ -1,6 +1,9 @@
-// const fs = require('fs');
+const fs = require('fs');
 
+// Block for synchronius reading
+let rawdata = fs.readFileSync('./data/mixed.json');
+let obj = JSON.parse(rawdata);
 
-exports.entries = function() {
-    return 'hello';
+exports.entries = function() { 
+    return obj;
 };
