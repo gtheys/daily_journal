d7e4a60954b36b6293654fd1b9b819ae6a193a5f
Geert Theys <geert.theys@gmail.com>
2018-03-20 15:16:40 +0700

    changed entries to journal to can have an entry

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const bodyParser = require('body-parser');
 
 const index = require('./routes/index');
 const users = require('./routes/users');
-const entries = require('./routes/entries');
+const journal = require('./routes/journal');
 
 const app = express();
 
@@ -24,7 +24,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
 app.use('/users', users);
-app.use('/entries', entries);
+app.use('/journal', journal);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ const router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.redirect('/entries');
+  res.redirect('/journal');
 });
 
 module.exports = router;
