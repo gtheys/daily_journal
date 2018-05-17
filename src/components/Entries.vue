1aefdaab4905e704ba2d995afb2314a6a9c63aa2
Geert Theys <geert.theys@gmail.com>
2018-05-17 11:22:47 +0700

    rename helloworld to entries

---
+++ src/components/Entries.vue
@@ -0,0 +1,26 @@
+<template>
+  <v-app>
+    <v-content>
+      <v-container
+        v-for="entry in myJson.entries"
+        :key="entry.id"
+      >
+        <v-icon>mdi-anchor</v-icon>
+        {{ entry.text }}
+      </v-container>
+    </v-content>
+  </v-app>
+</template>
+
+<script>
+
+import json from '../../data/Mixed.json';
+
+export default{
+  data() {
+    return {
+      myJson: json,
+    };
+  },
+};
+</script>
